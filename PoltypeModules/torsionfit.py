fb4301bbca4c5e95acfb7fffa2ff67dce98c1088
bdw2292 <bdw2292@utexas.edu>
2020-07-28 09:49:05 -0500

    log name fix

--- PoltypeModules/torsionfit.py
+++ PoltypeModules/torsionfit.py
@@ -126,9 +126,7 @@ def compute_qm_tor_energy(poltype,a,b,c,d,startangle,phase_list = None):
     WBOarray=[]
     for phaseangle in phase_list:
         angle = (startangle + phaseangle) % 360
-        if poltype.use_gaus:
-
-            minstrctfname = '%s-sp-%d-%d-%d-%d-%03d.log' % (poltype.molecprefix,a,b,c,d,round(angle))
+        minstrctfname = '%s-sp-%d-%d-%d-%d-%03d.log' % (poltype.molecprefix,a,b,c,d,round(angle))
         if not os.path.exists(minstrctfname): # if optimization failed then SP file will not exist
             tor_energy=None
             WBOvalue=None
