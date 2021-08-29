09a98bddfbe7ee32772b5d48f668de124bcd0585
bdw2292 <bdw2292@utexas.edu>
2021-08-29 15:32:08 -0500

    Update syntax for interfacing with RenLabCluster

--- PoltypeModules/apicall.py
+++ PoltypeModules/apicall.py
@@ -2,17 +2,21 @@ import os
 import sys
 import subprocess
 
-def CallExternalAPI(poltype,jobtoinputfilepaths,jobtooutputfiles,jobtoabsolutebinpath,scratchdir,jobinfofilenameprefix):
+def CallExternalAPI(poltype,jobtoinputfilepaths,jobtooutputfiles,jobtoabsolutebinpath,scratchdir,jobinfofilenameprefix,jobtooutputfilepath={}):
     poltype.WriteToLog('Calling external API ')
     jobinfofilepath=jobinfofilenameprefix+'.txt'
     temp=open(jobinfofilepath,'w')
-    head,tail=os.path.split(scratchdir)
     for job,inputfilepaths in jobtoinputfilepaths.items():
+        if len(jobtooutputfilepath)==0:
+            outputfilepath=os.path.split(inputfilepaths[0])[0]
+        else:
+            outputfilepath=jobtooutputfilepath[job]
         outputfiles=jobtooutputfiles[job]
+        outputfiles=[os.path.join(outputfilepath,i) for i in outputfiles]
         inputfilestr=','.join(inputfilepaths)
         outputfilestr=','.join(outputfiles)
         binpath=jobtoabsolutebinpath[job]
-        temp.write('--job='+job+' '+'--scratchdir='+tail+' '+'--scratchpath='+head+' '+'--scratchspace='+poltype.maxdisk+' '+'--numproc='+str(poltype.numproc)+' '+'--ram='+poltype.maxmem+' '+'--inputfilepaths='+inputfilestr+' '+'--outputfiles='+outputfilestr+' '+'--absolutepathtobin='+binpath+'\n')
+        temp.write('--job='+job+' '+'--scratchpath='+scratchdir+' '+'--numproc='+str(poltype.numproc)+' '+'--ram='+poltype.maxmem+' '+'--inputfilepaths='+inputfilestr+' '+'--outputfilepaths='+outputfilestr+' '+'--absolutepathtobin='+binpath+'\n')
     temp.close()
     if poltype.bashrcpath!=None:
         cmdstr='python'+' '+poltype.externalapi+' '+'--bashrcpath='+poltype.bashrcpath+' '+'--jobinfofilepath='+jobinfofilepath
