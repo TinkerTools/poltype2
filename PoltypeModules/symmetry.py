3a3e5d7c017cc352bf89441ec43f77d80ecd5bbb
bdw2292 <bdw2292@utexas.edu>
2022-04-25 12:07:43 -0500

    Refine symmetry typing topological distance invarient.

--- PoltypeModules/symmetry.py
+++ PoltypeModules/symmetry.py
@@ -4,6 +4,8 @@ from openbabel import openbabel
 from rdkit import Chem
 import numpy as np
 from openbabel import pybel
+import databaseparser
+
 
 def gen_canonicallabels(poltype,mol,rdkitmol=None):
     if rdkitmol==None:
@@ -31,7 +33,6 @@ def gen_canonicallabels(poltype,mol,rdkitmol=None):
             groups.append(set(matchingindices))
             grouptoheavy[tuple(set(matchingindices))]=heavy
         
-        
     sortedgroups=[]
     for group,heavy in grouptoheavy.items():
         if heavy==True:
@@ -57,9 +58,10 @@ def gen_canonicallabels(poltype,mol,rdkitmol=None):
 def ComputeSymmetryTypes(poltype,distmat,rdkitmol,mol):
     indextomatchingindices={}
     indextoGI={}
+    atomindices=databaseparser.RingAtomicIndices(poltype,mol)
     for atom in rdkitmol.GetAtoms():
         atomidx=atom.GetIdx()
-        GI=ComputeGIVector(poltype,atom,rdkitmol,distmat,mol)
+        GI=ComputeGIVector(poltype,atom,rdkitmol,distmat,mol,atomindices)
         indextoGI[atomidx]=GI
 
     for index,GI in indextoGI.items():
@@ -73,13 +75,34 @@ def ComputeSymmetryTypes(poltype,distmat,rdkitmol,mol):
     return indextomatchingindices
 
 
-def ComputeGIVector(poltype,atom,rdkitmol,distmat,mol):
+def ComputeGIVector(poltype,atom,rdkitmol,distmat,mol,atomindices):
     GI=[]
     atomidx=atom.GetIdx()
     distances=distmat[atomidx]
-    maxdist=max(distances)
+    indices=np.array(list(range(len(distances))))
+    indextodist=dict(zip(indices,distances))
+    newindextodist={}
+    for index,dist in indextodist.items():
+        oatom=rdkitmol.GetAtomWithIdx(int(index))
+        atomicnum=oatom.GetAtomicNum()
+        if atomicnum!=1:
+            newindextodist[index]=dist
+
+    newdistances=list(newindextodist.values())
+    dtocount={}
+    for d in newdistances:
+        count=list(newdistances).count(d)
+        dtocount[d]=count
+
+    sorteddtocount=dict(sorted(dtocount.items()))
+    distinv=[]
+    for d,count in sorteddtocount.items():
+        distinv.append(d*count)
+    maxdist=max(newdistances)
     GI.append(maxdist)
-    numneighbs=len([natom for natom in atom.GetNeighbors()])
+    GI.extend(distinv)
+    neighbs=[natom for natom in atom.GetNeighbors()]
+    numneighbs=len(neighbs)
     neighbatmnums=0
     for natom in atom.GetNeighbors():
         natomicnum=natom.GetAtomicNum()
@@ -98,5 +121,15 @@ def ComputeGIVector(poltype,atom,rdkitmol,distmat,mol):
     atomicnum=atom.GetAtomicNum()
     GI.append(atomicnum)
     GI.append(neighbatmnums)
-
+    if numneighbs==1:
+        neighb=neighbs[0]
+        natomidx=neighb.GetIdx()
+        rings=databaseparser.GrabAllRingsContainingIndices(poltype,atomindices,[natomidx+1])
+    else:
+        rings=databaseparser.GrabAllRingsContainingIndices(poltype,atomindices,[atomidx+1])
+    ringlens=[]
+    if len(rings)>0:
+        for ring in rings:
+            ringlens.append(len(ring))
+    GI.extend(ringlens)
     return GI
