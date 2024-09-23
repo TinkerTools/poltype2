ad6ee387659b123c0e8bdcba30241873f5e725b8
TimotheMelin <timothe-melin33@hotmail.fr>
2024-09-23 11:02:55 +0200

    Finish up first draft of script to fragment triple bond molecule

--- PoltypeModules/lFragmenterForDMA.py
+++ PoltypeModules/lFragmenterForDMA.py
@@ -220,29 +220,29 @@ def growFragment(atomidx, sdffile):
 
   # Case when atom is part of triple bond
   if (atomidx -1) in atomsInTripleBond:
-    print('hey')
     atomsToAdd = []
-    atomsToKeep = []
+    atomsToKeep = [atomidx-1]
     t1 = mol.GetAtomWithIdx(atomidx-1)
     for neig in t1.GetNeighbors():
       neig_idx = neig.GetIdx()
       atomsToAdd.append(neig_idx)
       atomsToKeep.append(neig_idx)
     
-
+    # Loop through the atoms to add and check:
+    # if the atom is in an aromatic ring
+    # if not, then try to find coonected atoms with specific rules
     for neig in atomsToAdd:
+
       if neig in atomsInRing:
         connectedAtoms = findConnectedAtoms(mol, atomsToAdd, atomsInRing)
-        print(connectedAtoms)
         atomsToKeep += connectedAtoms
+
       else:
-
         connectedAtoms = findConnectedAtomsGeneral(mol, atomsToAdd, atomidx, neig)
-        print(connectedAtoms)
-        print(atomsToKeep)
         atomsToKeep += connectedAtoms
-    saveFragment(mol, atomsToKeep, f"Frag_Atom{atomidx:03d}.mol")
 
+
+    saveFragment(mol, atomsToKeep, f"Frag_Atom{atomidx:03d}.mol")
     if os.path.isfile(f"Frag_Atom{atomidx:03d}_0.mol"):
       testmol = Chem.MolFromMolFile(f"Frag_Atom{atomidx:03d}.mol",removeHs=False)
       if len(testmol.GetAtoms()) == len(mol.GetAtoms()):
@@ -250,47 +250,118 @@ def growFragment(atomidx, sdffile):
       else:
         os.system(f"rm -f Frag_Atom{atomidx:03d}_0.mol")
 
-      
-
-
   return 
+
+
+def find_patern(mol,atm_to_check,pat_to_check):
+
+  """
+  This function find the atoms present in a specific pattern
+  and check if a given atom is part of the atom present in the 
+  pattern.
+
+  Inputs:
+    -   mol: rdkit mol object
+    -   atm_to_check: rdkit atom id to check
+    -   pat_to_check: SMART string to check as pattern
+
+  Outputs:
+    -   is_present: if atom_to_check exist or not in the SMART pattern (bool)
+    -   Search_pattern: list of rdkit atom ID present in the pattern
+  """
+
+
+  Search_pattern = []
+  pattern = Chem.MolFromSmarts(pat_to_check)
+  matches = mol.GetSubstructMatches(pattern)
+  for match in matches:
+    if len(match) == 1:
+      Search_pattern.append(match[0])
+
+  if atm_to_check in Search_pattern:
+    is_present = True
+  else:
+    is_present = False
+  
+  return is_present, Search_pattern
 
 
 def findConnectedAtomsGeneral(mol, atomsToadd, atomidx, neig): 
 
-  print('neigh to check for', neig)
+  """
+  This function attempt to find atoms connected to triple bond.
+  By default, it will cut just after an atom that does not have triple bond:
+  if the atom is part of triple bond, then stop after max two iterations.
+
+  TODO:
+    - If atoms neig as more than 1 neighboor, then need to add more options
+
+  Inputs:
+    -   mol: RDKIT mol object
+    -   atomsToadd: list of atom neighboor to main atom to check
+    -   atomidx: main atom to check ID
+    -   neig: neighboor atom of the main atom to check
+
+  Outputs:
+    -   Co_atoms: list of connected atoms
+  """
+
   Break = False
   prev_atm_idx = atomidx - 1
   current_atm_idx = neig
-
   Co_atoms = []
-  while not Break:
-    print('hello')
-    Neig = get_neigh_atoms(mol, current_atm_idx, prev_atm_idx)
-    print('Dict',Neig)
+  cc = 0
 
-    if len(Neig) == 1:
-        Break = False
-        print('Check dict')
-        print(Neig.keys)
+  # While loop until the check for connected atoms stops
+  while not Break:
+
+    # Grab the neighboor atoms of neig
+    Neig = get_neigh_atoms(mol, current_atm_idx, prev_atm_idx)
+
+    if len(Neig) == 1 and list(Neig.keys())[0] not in Co_atoms:
         prev_atm_idx = current_atm_idx
         current_atm_idx = list(Neig.keys())[0]
+
+        if find_patern(mol,current_atm_idx,'[*;R]')[0]:
+          atomsToAdd = list(Neig.keys())
+          connectedAtoms = findConnectedAtoms(mol, atomsToAdd, find_patern(mol,current_atm_idx,'[*;R]')[1])
+          atomsToAdd += connectedAtoms
+          Co_atoms += atomsToAdd
+          Break = True
+        else:
+          Co_atoms += list(Neig.keys())
+          Break = False
+
     else:
+        Co_atoms += list(Neig.keys())
         Break = True
 
-    Co_atoms += list(Neig.keys())
-
+    cc += 1
+    if cc == 2:
+        Break = True
 
   return Co_atoms
 
 
 def get_neigh_atoms(mol, Current_Idx, Prev_Idx):
-  print('CUrrent Idx',Current_Idx)
-  print('Prev Idx',Prev_Idx)
+
+  """
+  This function create a dictionnary such as:
+  {Atom ID: Atomic Num}
+  Make sure that none of previous atom are selected
+
+  Inputs:
+    -   mol: RDKIT mol object
+    -   Current_Idx: Atom id to check
+    -   Prev_Idx: Atom id of the previous atom (to skip)
+
+  Outputs:
+    -   Neighboor: dictionnary with the neighboored atoms 
+  
+  """
   C_atm = mol.GetAtomWithIdx(Current_Idx)
 
   Neighboor = {}
-
   for neib in C_atm.GetNeighbors():
     if neib.GetIdx() != Prev_Idx:
       IDx = mol.GetAtomWithIdx(neib.GetIdx())
