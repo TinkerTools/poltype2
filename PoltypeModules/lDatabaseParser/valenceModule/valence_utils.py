224456eb62de60a9e715c313d8307ba3822708cc
Chengwen Liu <liuchw2010@gmail.com>
2024-12-09 15:08:07 -0600

    deal with Amide/Aniline opbend

--- PoltypeModules/lDatabaseParser/valenceModule/valence_utils.py
+++ PoltypeModules/lDatabaseParser/valenceModule/valence_utils.py
@@ -35,5 +35,44 @@ def findSp2AtomTypes(txyz, sdffile):
     if (atom.GetHybridization() == Chem.HybridizationType.SP2) and (g.degree[i+1] == 3):
       if str(atom2type[i+1]) not in sp2AtomTypes:
         sp2AtomTypes.append(str(atom2type[i+1]))
+  
+  # Detect Nitrogen of Amide
+  amide_nitrogens = [] 
+  pattern = Chem.MolFromSmarts('[#6](=O)[#7X3]')
+  matches = rdkitmol.GetSubstructMatches(pattern, uniquify=False)
+  for match in matches:
+    amide_nitrogens.append(match[2])
+  
+  amide_nitrogen_types = []
+  for n in amide_nitrogens:
+    atype = atom2type[n+1]
+    if atype not in amide_nitrogen_types:
+      amide_nitrogen_types.append(atype)
+
+  # Detect Nitrogen of Aniline-like molecule
+  aniline_like_nitrogens = [] 
+  pattern = Chem.MolFromSmarts('[a][#7X3]')
+  matches = rdkitmol.GetSubstructMatches(pattern, uniquify=False)
+  for match in matches:
+    aniline_like_nitrogens.append(match[1])
+  
+  aniline_like_nitrogen_types = []
+  for n in aniline_like_nitrogens:
+    atype = atom2type[n+1]
+    if atype not in aniline_like_nitrogen_types:
+      aniline_like_nitrogen_types.append(atype)
+  
+  # N of Aniline-like molecule DONOT need opbend/anglep
+  for n in aniline_like_nitrogen_types:
+    if str(n) in sp2AtomTypes:
+      sp2AtomTypes.remove(str(n))
+      print(f'Removing type {str(n)} from SP2 atoms since it is an aniline_like_nitrogen')
+  
+  # N of Amide DO need opbend/anglep
+  for n in amide_nitrogen_types:
+    if str(n) not in sp2AtomTypes:
+      sp2AtomTypes.append(str(n))
+      print(f'Adding type {str(n)} to SP2 atoms since it is an amide_nitrogen')
+
   return sp2AtomTypes
 
--- PoltypeModules/ldatabaseparser.py
+++ PoltypeModules/ldatabaseparser.py
@@ -136,6 +136,54 @@ def write_initial_parameters(sdffile, txyz):
         edges.append(s)
   g.add_nodes_from(nodes)
   g.add_edges_from(edges)
+  
+  # Find the tri- center
+  tricentertypes = []
+  rdkitmol = Chem.MolFromMolFile(sdffile,removeHs=False)
+  num_atoms = rdkitmol.GetNumAtoms() 
+  for i in range(num_atoms):
+    atom = rdkitmol.GetAtomWithIdx(i)
+    if (atom.GetHybridization() == Chem.HybridizationType.SP2) and (g.degree[i+1] == 3):
+      if str(atom2type[i+1]) not in tricentertypes: 
+        tricentertypes.append(str(atom2type[i+1]))
+ 
+  # Detect Nitrogen of Amide
+  amide_nitrogens = [] 
+  pattern = Chem.MolFromSmarts('[#6](=O)[#7X3]')
+  matches = rdkitmol.GetSubstructMatches(pattern, uniquify=False)
+  for match in matches:
+    amide_nitrogens.append(match[2])
+  
+  amide_nitrogen_types = []
+  for n in amide_nitrogens:
+    atype = atom2type[n+1]
+    if atype not in amide_nitrogen_types:
+      amide_nitrogen_types.append(atype)
+
+  # Detect Nitrogen of Aniline-like molecule
+  aniline_like_nitrogens = [] 
+  pattern = Chem.MolFromSmarts('[a][#7X3]')
+  matches = rdkitmol.GetSubstructMatches(pattern, uniquify=False)
+  for match in matches:
+    aniline_like_nitrogens.append(match[1])
+  
+  aniline_like_nitrogen_types = []
+  for n in aniline_like_nitrogens:
+    atype = atom2type[n+1]
+    if atype not in aniline_like_nitrogen_types:
+      aniline_like_nitrogen_types.append(atype)
+  
+  # N of Aniline-like molecule DONOT need opbend/anglep
+  for n in aniline_like_nitrogen_types:
+    if str(n) in tricentertypes:
+      tricentertypes.remove(str(n))
+      print(f'Removing type {str(n)} from SP2 atoms since it is an aniline_like_nitrogen')
+  
+  # N of Amide DO need opbend/anglep
+  for n in amide_nitrogen_types:
+    if str(n) not in tricentertypes:
+      tricentertypes.append(str(n))
+      print(f'Adding type {str(n)} to SP2 atoms since it is an amide_nitrogen')
 
   # find bonds and angles
   bonds = {} 
@@ -167,16 +215,7 @@ def write_initial_parameters(sdffile, txyz):
           else:
             angles[comb] += [a]
   
-  # find the tri- center
-  tricentertypes = []
-  rdkitmol = Chem.MolFromMolFile(sdffile,removeHs=False)
-  num_atoms = rdkitmol.GetNumAtoms() 
-  for i in range(num_atoms):
-    atom = rdkitmol.GetAtomWithIdx(i)
-    if (atom.GetHybridization() == Chem.HybridizationType.SP2) and (g.degree[i+1] == 3):
-      if str(atom2type[i+1]) not in tricentertypes: 
-        tricentertypes.append(str(atom2type[i+1]))
-  
+
   with open("valence_init.dat", 'w') as f:
     for bond in bonds:
       b = bond.split('-') 
